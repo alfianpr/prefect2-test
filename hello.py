25e658e2392f56cb51b1032175d760aa3eccbe2a
alfianpr <alfian.pratama@kalbenutritionals.com>
2023-09-05 19:04:08 +0700

    update

--- hello.py
+++ hello.py
@@ -1,6 +1,14 @@
-from prefect import flow
+from prefect import flow, task
+
+@task
+def create_message():
+    msg = "hello, prefect!"
+    return msg
 
 @flow
 def hello_world():
-    print("Hello world!")
+    return print(create_message())
 
+
+if __name__ == "__main__":
+    hello_world()
